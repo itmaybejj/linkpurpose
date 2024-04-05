2e1b14af1a34a2f8e6d261276cf9d63d495702b6
John Jameson <jjameson@princeton.edu>
2024-04-05 16:31:15 -0400

    remove redundant strings

--- js/linkpurpose.js
+++ js/linkpurpose.js
@@ -85,7 +85,8 @@ class LinkPurpose {
           iconType: 'html',
           iconPosition: 'beforeend',
           iconHTML: '<svg class="linkpurpose-default-svg" width="14" height="14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>',
-          iconClasses: ['fa-solid', 'fa-download'] // set iconType to classes to use
+          iconClasses: ['fa-solid', 'fa-download'], // set iconType to classes to use
+          redundantStrings: /(download)/,
         },
 
         document: {
@@ -97,7 +98,8 @@ class LinkPurpose {
           iconType: 'html',
           iconPosition: 'beforeend',
           iconHTML: '<svg class="linkpurpose-default-svg" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>',
-          iconClasses: ['fa-regular', 'fa-file-lines'] // set iconType to classes to use
+          iconClasses: ['fa-regular', 'fa-file-lines'], // set iconType to classes to use
+          redundantStrings: /%s(download|file|word|excel|pdf)/,
         },
 
         // Ref www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
@@ -320,6 +322,11 @@ class LinkPurpose {
 
       marks.forEach((mark) => {
         mark.hits.forEach((hit, i) => {
+          // Don't append redundant screen reader text.
+          let showText = !(LinkPurpose.options.purposes[hit.type].redundantStrings &&
+            mark.link.textContent.length > 0 &&
+            mark.link.textContent.match(LinkPurpose.options.purposes[hit.type].redundantStrings));
+
           if (i === 0) {
             let spanTarget = mark.link
 
@@ -393,9 +400,9 @@ class LinkPurpose {
             if (LinkPurpose.options.hiddenTextClass) {
               iconText.classList.add(LinkPurpose.options.hiddenTextClass);
             }
-            iconText.textContent = `(${LinkPurpose.options.purposes[hit.type].message})`;
+            iconText.textContent = showText ? `(${LinkPurpose.options.purposes[hit.type].message})` : '';
             spanTarget.append(iconText);
-          } else {
+          } else if (showText) {
             const iconText = mark.link.querySelector('.link-purpose-text')
             iconText.classList.add(LinkPurpose.options.purposes[hit.type].iconWrapperClass)
             iconText.textContent = iconText.textContent + LinkPurpose.options.purposes[hit.type].message
