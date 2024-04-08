d0d6f50c0f5d18e2a3194b66236ea0bb629a41a3
John Jameson <jjameson@princeton.edu>
2024-04-08 10:41:27 -0400

    Hide redundant screen reader text.

--- js/linkpurpose.js
+++ js/linkpurpose.js
@@ -61,7 +61,7 @@ class LinkPurpose {
           iconPosition: 'beforeend', // beforebegin, afterbegin, beforeend, afterend
           iconHTML: '<svg class="linkpurpose-default-svg" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M432 48H208c-17.7 0-32 14.3-32 32V96H128V80c0-44.2 35.8-80 80-80H432c44.2 0 80 35.8 80 80V304c0 44.2-35.8 80-80 80H416V336h16c17.7 0 32-14.3 32-32V80c0-17.7-14.3-32-32-32zM48 448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V256H48V448zM64 128H320c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192c0-35.3 28.7-64 64-64z"/></svg>',
           iconClasses: ['fa-regular', 'fa-window-restore'], // Goes on icon span only if iconType is "classes"
-          redundantStrings: /(window|\stab)/, // Screen reader text will not be inserted if present
+          redundantStrings: /(window|\stab)/i, // Screen reader text will not be inserted if present
         },
 
         external: {
@@ -89,7 +89,7 @@ class LinkPurpose {
           iconPosition: 'beforeend',
           iconHTML: '<svg class="linkpurpose-default-svg" width="14" height="14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>',
           iconClasses: ['fa-solid', 'fa-download'], // set iconType to classes to use
-          redundantStrings: /(download)/,
+          redundantStrings: /(download)/i,
         },
 
         document: {
@@ -102,7 +102,7 @@ class LinkPurpose {
           iconPosition: 'beforeend',
           iconHTML: '<svg class="linkpurpose-default-svg" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/></svg>',
           iconClasses: ['fa-regular', 'fa-file-lines'], // set iconType to classes to use
-          redundantStrings: /(download|file|word|excel|pdf|xls|powerpoint|docx|([.(]|\s)(ppt|doc))/,
+          redundantStrings: /(download|file|word|excel|pdf|xls|powerpoint|docx|([.(]|\s)(ppt|doc))/i,
         },
 
         // Ref www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
@@ -131,7 +131,7 @@ class LinkPurpose {
           iconPosition: 'beforeend',
           iconHTML: '<svg class="linkpurpose-default-svg" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="Currentcolor" d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>',
           iconClasses: ['fa-regular', 'fa-envelope'], // set iconType to classes to use
-          redundantStrings: /@|mail/,
+          redundantStrings: /@|mail/i,
         },
 
         tel: {
@@ -414,7 +414,7 @@ class LinkPurpose {
           } else if (showText) {
             const iconText = mark.link.querySelector('.link-purpose-text')
             iconText.classList.add(LinkPurpose.options.purposes[hit.type].iconWrapperClass)
-            iconText.textContent = iconText.textContent + LinkPurpose.options.purposes[hit.type].message
+            iconText.textContent = iconText.textContent + ` (${LinkPurpose.options.purposes[hit.type].message})`
           }
           if (LinkPurpose.options.purposes[hit.type].newWindow) {
             mark.link.setAttribute('target', '_blank')
